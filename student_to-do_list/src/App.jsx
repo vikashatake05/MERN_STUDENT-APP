6fa2b0154fbf16f9ae9359de3d925a5559eae951
vikashatake05 <vikashatake05@users.noreply.github.com>
2026-09-08 16:30:19 +0530

    day 2 of MERN

---
+++ student_to-do_list/src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ student_to-do_list/src/components/Welcome.jsx
@@ -0,0 +1,4 @@
+function Welcome() {
+return (<h1>Welcome to the To-Do List</h1>);
+}
+export default Welcome;
